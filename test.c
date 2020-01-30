cb77a7f18d1b2c3de8bb4799d3a36e0676791e62
Darrell Long <darrell.long@gmail.com>
2020-01-29 21:47:59 -0800

    Options

--- test.c
+++ test.c
@@ -1,8 +1,28 @@
 #include "queue.h"
-#include <stdio.h>
 
-int main(void) {
-  queue *q = newQueue(100);
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <inttypes.h>
+
+#define OPTIONS "n:"
+
+int main(int argc, char **argv) {
+  uint32_t n = 100;
+
+  int c;
+  while ((c = getopt(argc, argv, OPTIONS)) != -1) {
+    switch (c) {
+    case 'n':
+      n = strtol(optarg, (char **)NULL, 10);
+      break;
+    default:
+      printf("Invalid argument\n");
+      break;
+    }
+  }
+
+  queue *q = newQueue(n);
 
   for (uint32_t i = 0; !fullQ(q); i += 1) {
     (void) enqueue(q, &i);
